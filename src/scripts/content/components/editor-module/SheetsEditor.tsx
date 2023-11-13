75cb9c0de425ee503f692aa30830fa7b032e22b8
git-chad <tobimocc@gmail.com>
2023-11-13 15:57:23 -0300

    fix(dev): downgraded to codemirror v5

--- src/scripts/content/components/editor-module/SheetsEditor.tsx
+++ src/scripts/content/components/editor-module/SheetsEditor.tsx
@@ -1,71 +1,21 @@
-// import React, { useEffect, useRef } from 'react';
-// import { EditorState } from '@/utils/Libraries';
-// import { EditorView } from '@/utils/Libraries';
-// import { autocompletion, closeBrackets, closeBracketsKeymap, completionKeymap } from '@/utils/Libraries';
-// import { defaultKeymap, history, historyKeymap } from '@/utils/Libraries';
-// import { javascript } from '@/utils/Libraries';
-// import { bracketMatching, defaultHighlightStyle, foldGutter, foldKeymap, indentOnInput, syntaxHighlighting } from '@/utils/Libraries';
-// import { lintKeymap } from '@/utils/Libraries';
-// import { highlightSelectionMatches, searchKeymap } from '@/utils/Libraries';
-// import { crosshairCursor, drawSelection, dropCursor, highlightActiveLine, highlightActiveLineGutter, highlightSpecialChars, keymap, lineNumbers, rectangularSelection } from '@/utils/Libraries';
-// import {spreadsheet} from "@/utils/Libraries";
+import React, { useEffect, useRef } from 'react';
+import '@/lib/codemirror-5.65.15/lib/codemirror.css';
+import '@/lib/codemirror-5.65.15/lib/codemirror.js';
+import '@/lib/codemirror-5.65.15/mode/spreadsheet/spreadsheet.js';
 
+const SheetsEditor = () => {
+  const editorRef = useRef(null);
 
-// interface CodeMirrorEditorProps {
-//   initialDoc?: string;
-// }
+  useEffect(() => {
+    if (window.CodeMirror && editorRef.current) {
+      const editor = window.CodeMirror(editorRef.current, {
+        lineNumbers: true,
+        mode: 'spreadsheet'
+      });
+    }
+  }, []);
 
-// const CodeMirrorEditor: React.FC<CodeMirrorEditorProps> = ({ initialDoc = 'console.log("hello, world")' }) => {
-//   const editorRef = useRef<HTMLDivElement>(null);
+  return <div ref={editorRef}></div>;
+};
 
-//   useEffect(() => {
-//     if (editorRef.current) {
-//       const startState = EditorState.create({
-//         doc: initialDoc,
-//         extensions: [
-//           lineNumbers(),
-//           highlightActiveLineGutter(),
-//           highlightSpecialChars(),
-//           history(),
-//           foldGutter(),
-//           drawSelection(),
-//           dropCursor(),
-//           EditorState.allowMultipleSelections.of(true),
-//           indentOnInput(),
-//           syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-//           bracketMatching(),
-//           closeBrackets(),
-//           autocompletion(),
-//           rectangularSelection(),
-//           crosshairCursor(),
-//           highlightActiveLine(),
-//           highlightSelectionMatches(),
-//           keymap.of([
-//             ...closeBracketsKeymap,
-//             ...defaultKeymap,
-//             ...searchKeymap,
-//             ...historyKeymap,
-//             ...foldKeymap,
-//             ...completionKeymap,
-//             ...lintKeymap,
-//           ]),
-//           javascript(),
-//         ],
-//       });
-
-//       const view = new EditorView({
-//         state: startState,
-//         parent: editorRef.current,
-//       });
-
-//       return () => {
-//         view.destroy();
-//       };
-//     }
-//   }, [initialDoc]);
-
-//   return <div ref={editorRef} />;
-// };
-
-// export default CodeMirrorEditor;
-
+export default SheetsEditor;
