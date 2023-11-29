719df2a19d4caf16717140fbfb899505f6b39103
git-chad <tobimocc@gmail.com>
2023-11-29 10:41:33 -0300

    t

--- src/scripts/content/components/popup-module/CustomPopup.tsx
+++ src/scripts/content/components/popup-module/CustomPopup.tsx
@@ -5,15 +5,16 @@ import SheetsEditor from '../editor-module/SheetsEditor'
 const CustomPopup = ({ showPopup }) => {
     const [isExpanded, setIsExpanded] = useState(false)
     const [darkMode, setDarkMode] = useState(false)
+    const [prettifyFunction, setPrettifyFunction] = useState(() => () => {})
 
     // Determine the theme name based on darkMode
-    const themeName = darkMode ? 'monokai' : 'paraiso-light';
+    const themeName = darkMode ? 'monokai' : 'paraiso-light'
 
     useEffect(() => {
         // Update the body class
-        const themeClass = darkMode ? 'dark' : 'light';
-        document.body.classList.remove('dark', 'light');
-        document.body.classList.add(themeClass);
+        const themeClass = darkMode ? 'dark' : 'light'
+        document.body.classList.remove('dark', 'light')
+        document.body.classList.add(themeClass)
 
         // Any other theme-related side effects can go here
     }, [darkMode])
@@ -21,7 +22,11 @@ const CustomPopup = ({ showPopup }) => {
     const handleExpandToggle = () => {
         setIsExpanded(!isExpanded)
     }
-// bg-[#141414]
+
+    const handlePrettifyFunction = func => {
+        setPrettifyFunction(() => func)
+    }
+
     return (
         <div
             className={`fixed 2xl:bottom-28 2xl:right-8 bottom-24 right-3 origin-right ${
@@ -30,9 +35,8 @@ const CustomPopup = ({ showPopup }) => {
                 showPopup ? 'scale-100 opacity-100' : 'scale-95 opacity-0 pointer-events-none'
             } ${darkMode ? '!bg-[#141414] !text-fswhite' : ''}`}
         >
-
             {/* Pass the themeName prop to SheetsEditor */}
-            <SheetsEditor themeName={themeName}/>
+            <SheetsEditor themeName={themeName} onPrettifyFunctionReady={handlePrettifyFunction} />
 
             {/* Bottom Navbar */}
             <InternalNavbar
@@ -40,6 +44,7 @@ const CustomPopup = ({ showPopup }) => {
                 toggleDarkMode={() => setDarkMode(prevMode => !prevMode)}
                 onExpandToggle={handleExpandToggle}
                 isExpanded={isExpanded}
+                prettify={prettifyFunction} 
             />
         </div>
     )
