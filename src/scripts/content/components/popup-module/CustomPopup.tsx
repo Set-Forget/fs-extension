314f045431ae17308bf84563000feaf9a6bd2d4d
Juan Canneva <jcanneva@MacBook-Pro-de-Juan.local>
2023-12-27 13:07:07 -0300

    added drag in x

--- src/scripts/content/components/popup-module/CustomPopup.tsx
+++ src/scripts/content/components/popup-module/CustomPopup.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import InternalNavbar from './InternalNavbar'
 import SheetsEditor from '../editor-module/SheetsEditor'
+import { ONE_HUNDRED, ONE_THOUSAND } from '@/utils/constants'
 
 const CustomPopup = ({ showPopup }) => {
     const [isExpanded, setIsExpanded] = useState(false)
     const [darkMode, setDarkMode] = useState(false)
     const [isFormatted, setIsFormatted] = useState<boolean>(false)
+    const [positionX, setPositionX] = useState<string>('')
     const [prettify, setPrettify] = useState(() => () => {})
 
     // Determine the theme name based on darkMode
@@ -21,6 +23,7 @@ const CustomPopup = ({ showPopup }) => {
     }, [darkMode])
 
     const handleExpandToggle = () => {
+        if (!isExpanded && Number(positionX) > ONE_THOUSAND) return
         setIsExpanded(!isExpanded)
     }
 
@@ -28,14 +31,18 @@ const CustomPopup = ({ showPopup }) => {
         setPrettify(() => func)
     }
 
+    const className = `fixed 2xl:bottom-28 2xl:right-8 bottom-24 right-3 origin-right ${
+        isExpanded ? 'w-[35vw] h-[85vh]' : 'w-[25vw] h-[65vh]'
+    } bg-white text-fsblack  shadow-lg rounded-lg transform transition-all duration-[400ms] flex flex-col ${
+        showPopup ? 'scale-100 opacity-100' : 'scale-95 opacity-0 pointer-events-none'
+    } ${darkMode ? '!bg-[#141414] !text-fswhite' : ''}`
+
     return (
         <div
-            //TODO consider move to a const
-            className={`fixed 2xl:bottom-28 2xl:right-8 bottom-24 right-3 origin-right ${
-                isExpanded ? 'w-[35vw] h-[85vh]' : 'w-[25vw] h-[65vh]'
-            } bg-white text-fsblack  shadow-lg rounded-lg transform transition-all duration-[400ms] flex flex-col ${
-                showPopup ? 'scale-100 opacity-100' : 'scale-95 opacity-0 pointer-events-none'
-            } ${darkMode ? '!bg-[#141414] !text-fswhite' : ''}`}
+            className={className}
+            draggable
+            onDragEnd={dragEnd}
+            style={{ right: `${positionX}px` }}
         >
             {/* Pass the themeName prop to SheetsEditor */}
             <SheetsEditor
@@ -56,6 +63,18 @@ const CustomPopup = ({ showPopup }) => {
             />
         </div>
     )
+
+    function dragEnd(ev) {
+        const { clientX } = ev
+        if (clientX < ONE_HUNDRED && !isExpanded) {
+            setPositionX('1100')
+        } else if (clientX > ONE_HUNDRED && clientX < ONE_THOUSAND) {
+            setPositionX('600')
+        } else if (clientX > ONE_THOUSAND) {
+            setPositionX('12')
+        }
+        ev.dataTransfer.setData('text', ev.target.id)
+    }
 }
 
 export default CustomPopup
